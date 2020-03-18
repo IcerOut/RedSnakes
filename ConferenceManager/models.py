406894d501442a3f0aeb1df5e2c4a1277d254165
IcerOut <44326661+IcerOut@users.noreply.github.com>
2020-03-18 12:59:23 +0200

    Prepared for model creation

--- ConferenceManager/models.py
+++ ConferenceManager/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Organization(models.Model):
+    OrganizationID = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return Organization
+
