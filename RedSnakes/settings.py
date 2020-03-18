342660a02abb07cf755663e94f6e6740d88e9ade
IcerOut <44326661+IcerOut@users.noreply.github.com>
2020-03-18 12:46:47 +0200

    Changed settings to work with MySQL, created initial setup script

--- RedSnakes/settings.py
+++ RedSnakes/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'RedSnakes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'redsnakes',
+        'USER': 'root',
+        'Password': '',
+        'HOST': '127.0.0.1',
+        'PORT': '10245'
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT+3'
 
 USE_I18N = True
 
---
+++ setup.bat
@@ -0,0 +1,8 @@
+@echo off
+pip install -r requirements.txt
+
+echo [91mNow install MySQL Workbench from https://dev.mysql.com/downloads/workbench/[0m
+echo [94mAfter it's installed, create a new database named "redsnakes" and run the create_tables .sql script on it[0m
+echo [95mAfter that is done, run the program from inside PyCharm and check if any exceptions occur or if you can access the default web page at http://127.0.0.1:8000/[0m
+
+pause
