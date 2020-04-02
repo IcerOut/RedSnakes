12ae0fadaa12bfe3e2eb00ce3ce4afe697a25c93
Hiticas Claudiu <hiticas.claudiu@gmail.com>
2020-04-02 20:05:05 +0300

    diagrams

--- RedSnakes/settings.py
+++ RedSnakes/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'RedSnakes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'redsnakes',
+        'USER': 'root',
+        'Password': '',
+        'HOST': '127.0.0.1',
+        'PORT': '10245'
     }
 }
 
