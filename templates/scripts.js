9c5b711a4b4ef9c12cd3917c8cf41d37cf9a3ef8
sekilm <31830095+sekilm@users.noreply.github.com>
2020-05-01 17:15:08 +0300

    Added bidding

--- templates/scripts.js
+++ templates/scripts.js
@@ -63,12 +63,12 @@ function getReviews() {
             let json = jsonParse(this.responseText);
 
             for(let i = 0; i < json.length; i++) {
-                let rev = json[i];
+                let review = json[i];
                 let row = new_tbody.insertRow();
-                Object.keys(rev).forEach(function (k) {
+                Object.keys(review).forEach(function (k) {
                     let text;
                     let cell = row.insertCell();
-                    text = rev[k];
+                    text = review[k];
                     cell.appendChild(document.createTextNode(text));
                 })
             }
@@ -77,4 +77,69 @@ function getReviews() {
     };
     http.open("POST", "api/reviews/getAll");
     http.send(null);
+}
+
+function getPapers() {
+    let http = new XMLHttpRequest();
+    http.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let table = document.getElementsByTagName("table")[0];
+            let old_tbody = document.getElementsByTagName("tbody")[0];
+            let new_tbody = document.createElement('tbody');
+
+            let json = jsonParse(this.responseText);
+
+            for(let i = 0; i < json.length; i++) {
+                let paper = json[i];
+
+                let row = new_tbody.insertRow();
+                let cell = row.insertCell();
+                cell.appendChild(document.createTextNode(paper["title"]));
+                cell = row.insertCell();
+                cell.appendChild(document.createTextNode(paper["abstract"]));
+
+                cell = row.insertCell();
+                let select = document.createElement("select");
+                select.id = "select-" + paper["id"];
+
+                let option1 = document.createElement("option");
+                option1.text = "Indifferent";
+                let option2 = document.createElement("option");
+                option2.text = "Review";
+                let option3 = document.createElement("option");
+                option3.text = "Do not review";
+
+                select.add(option1);
+                select.add(option2);
+                select.add(option3);
+
+                cell.appendChild(select);
+            }
+            table.replaceChild(new_tbody, old_tbody);
+        }
+    };
+    http.open("GET", "api/papers/getAll");
+    http.send(null);
+}
+
+function sendBidding() {
+    let combobox = document.getElementsByTagName("select");
+
+    let result = [];
+
+    for(let i = 0; i < combobox.length; i++) {
+        let id = combobox[i].id.substring(7);
+        let choice = combobox[i].value;
+
+        let dict = { id: id,
+                    choice: choice};
+
+        result.push(dict);
+    }
+
+    let json = JSON.stringify(result);
+
+    let http = new XMLHttpRequest();
+    http.open("POST", "api/review/sendbidding");
+    http.send(json);
 }
