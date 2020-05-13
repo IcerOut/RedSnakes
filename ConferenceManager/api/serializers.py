fb42626b546a0e2dfa2bb7575361337d709d5041
Alin Lung <lung.s.alin@gmail.com>
2020-05-13 20:31:58 +0300

    Update serializers.py

--- ConferenceManager/api/serializers.py
+++ ConferenceManager/api/serializers.py
@@ -1,17 +1,9 @@
 from rest_framework import serializers
 
-from ..models import Author, Paper
-
-class AuthorSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Author
-        fields = ('name', 'affiliation', 'email', 'title', 'conference', 'section')
+from ..models import Paper
 
 
 class PaperSerializer(serializers.HyperlinkedModelSerializer):
-    # Necessary for the many-to-many relation
-    author = AuthorSerializer(read_only=True, many=True)
-
     class Meta:
         model = Paper
-        fields = ('name', 'noPages', 'paperKind', 'evalDecision', 'conference', 'author')
+        fields = ('paperId', 'path', 'accepted')
