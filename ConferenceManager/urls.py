518fe049750c12fb636d45b7ffce5cdb49690dc0
Hiticas Claudiu <hiticas.claudiu@gmail.com>
2020-05-14 22:05:07 +0300

    add conference in database

---
+++ ConferenceManager/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from ConferenceManager import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('conference-list/', views.conference_list, name='conference_list'),
+    path('evaluation-results/', views.evaluation_results, name='evaluation_results'),
+    path('submit-new-conference/', views.add_new_conference, name='submit_new_conference'),
+    path('assign-reviewers/', views.assign_reviewers, name='assign_reviewers'),
+    path('bidding/', views.bidding, name='bidding'),
+    path('evaluation/', views.evaluation, name='evaluation'),
+    path('review/', views.review, name='review'),
+    path('section-choices/', views.section_choices, name='section_choices'),
+    path('split-papers-into-sections/', views.split_papers_into_sections, name='split_papers_into_sections'),
+]
--- ConferenceManager/views.py
+++ ConferenceManager/views.py
@@ -1,9 +1,35 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import View
+from ConferenceManager.models import *
 
+
+def add_new_conference(request):
+    conferences = Conference.objects.all()
+    response_data = {}
+
+    if request.POST.get('action') == 'post':
+        name = request.POST.get('name')
+        submissionDeadline = request.POST.get('submissionDeadline')
+        reviewDeadline = request.POST.get('reviewDeadline')
+        conferenceDate = request.POST.get('conferenceDate')
+
+        response_data['name'] = name
+        response_data['submissionDeadline'] = submissionDeadline
+        response_data['reviewDeadline'] = reviewDeadline
+        response_data['conferenceDate'] = conferenceDate
+
+        Conference.objects.create(
+            name = name,
+            submissionDeadline = submissionDeadline,
+            reviewDeadline = reviewDeadline,
+            conferenceDate = conferenceDate
+            )
+        return JsonResponse(response_data)
+
+    return render(request, 'submit-new-conference.html', {'conferences':conferences})
 
 def home(request):
     return render(request, "home.html")
@@ -14,8 +40,8 @@ def conference_list(request):
 def evaluation_results(request):
     return render(request, "evaluation-results.html")
 
-def submit_new_conference(request):
-    return render(request, "submit-new-conference.html")
+# def submit_new_conference(request):
+#     return render(request, "submit-new-conference.html")
 
 def assign_reviewers(request):
     return render(request, "assign-reviewers.html")
