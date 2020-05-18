1641d8c538f4182b6c1c45cf7889a94a7406272f
Alin Lung <lung.s.alin@gmail.com>
2020-05-18 18:44:40 +0300

    Create ReviewService.py

---
+++ RedSnakes/Service/ReviewService.py
@@ -0,0 +1,18 @@
+from RedSnakes.Service.MainService import MainService
+
+
+class ReviewService(MainService):
+    def __init__(self):
+        super()
+
+    def add(self, entity):
+        pass
+
+    def update(self, entity):
+        pass
+
+    def getAll(self):
+        pass
+
+    def delete(self, entity):
+        pass
