face3f670b86567fb6ebf2c5ef738f1a850847b0
Alin Lung <lung.s.alin@gmail.com>
2020-05-18 18:44:26 +0300

    Update serializers.py

--- ConferenceManager/serializers.py
+++ ConferenceManager/serializers.py
@@ -1,9 +1,45 @@
 from rest_framework import serializers
 
-from .models import Conference
+from .models import Abstract, Bid, Conference, Paper, ProgramCommitteeMember, Review
 
 
 class ConferenceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Conference
         fields = ('name', 'submissionDeadline', 'reviewDeadline', 'conferenceDate')
+
+
+class PaperSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Paper
+        fields = ('paperId', 'path', 'accepted')
+
+
+class ProgramCommitteeMemberSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ProgramCommitteeMember
+        fields = ('pEmail', 'cId', 'rank')
+
+
+class AbstractSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Abstract
+        fields = ('authorId', 'text', 'title')
+
+
+class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+    pcId = ProgramCommitteeMemberSerializer()
+    paperId = PaperSerializer()
+
+    class Meta:
+        model = Review
+        fields = ('paperId', 'pcId', 'status')
+
+
+class BidSerializer(serializers.HyperlinkedModelSerializer):
+    abstractId = AbstractSerializer()
+    pcId = ProgramCommitteeMemberSerializer()
+
+    class Meta:
+        model = Bid
+        fields = ('abstractId', 'pcId', 'status')
