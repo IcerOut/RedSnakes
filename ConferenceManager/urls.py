8af03670a8a744199123fb35d0bf9403be42bbc8
DeeaKr <korodiandreea@yahoo.com>
2020-05-21 14:56:06 +0300

    fixed

--- ConferenceManager/urls.py
+++ ConferenceManager/urls.py
@@ -14,13 +14,12 @@ urlpatterns = [
     path('review/', views.review, name='review'),
     path('section-choices/', views.section_choices, name='section_choices'),
     path('split-papers-into-sections/', views.split_papers_into_sections, name='split_papers_into_sections'),
-<<<<<<< HEAD
-=======
+
     path('chair-register/', views.chair_register, name='chair_register'),
     path('speaker-register/', views.speaker_register, name='speaker_register'),
     path('listner-register/', views.listener_register, name='listener_register'),
 
->>>>>>> 31cf69d94b75d27f4fdc17b0335034c9af2139aa
+
     path('api/conferences/getAll', api.conference_list, name='api_conference_list'),
     path('api/conferences/add', api.add_new_conference, name='add_new_conference'),
 ]
