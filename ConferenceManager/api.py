7528e9cb0d53442891ec51f256691ca2b0e40d7b
lorena1999 <lorena.marginean@yahoo.com>
2020-05-22 16:28:43 +0300

    File Server - upload commit

--- ConferenceManager/api.py
+++ ConferenceManager/api.py
@@ -1,156 +1,172 @@
 import traceback
 
-from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.http import HttpRequest , HttpResponse , JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from ConferenceManager import serializers
-from ConferenceManager.models import Conference, ConferenceAuthor, Participant
+from ConferenceManager.models import Conference , ConferenceAuthor , Participant
 from RedSnakes.Service.PapersService import PapersService
 from RedSnakes.Service.ReviewService import ReviewService
+
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 
 def conference_list(request):
     if request.method == 'GET':
-        conferences = Conference.objects.all()
-        conferences_json = serializers.ConferenceSerializer(conferences, many=True)
-        return JsonResponse(conferences_json.data, safe=False)
-    return HttpResponse(status=405)
+        conferences = Conference.objects.all ()
+        conferences_json = serializers.ConferenceSerializer ( conferences , many=True )
+        return JsonResponse ( conferences_json.data , safe=False )
+    return HttpResponse ( status=405 )
 
 
 def add_new_conference(request):
     response_data = {}
 
     if request.method == 'POST':
-        name = request.POST.get('name')
-        submissionDeadline = request.POST.get('submissionDeadline')
-        reviewDeadline = request.POST.get('reviewDeadline')
-        conferenceDate = request.POST.get('conferenceDate')
+        name = request.POST.get ( 'name' )
+        submissionDeadline = request.POST.get ( 'submissionDeadline' )
+        reviewDeadline = request.POST.get ( 'reviewDeadline' )
+        conferenceDate = request.POST.get ( 'conferenceDate' )
 
-        response_data = {'name': name, 'submissionDeadline': submissionDeadline,
-                         'reviewDeadline': reviewDeadline, 'conferenceDate': conferenceDate}
+        response_data = {'name': name , 'submissionDeadline': submissionDeadline ,
+                         'reviewDeadline': reviewDeadline , 'conferenceDate': conferenceDate}
 
-        Conference.objects.create(
-                name=name,
-                submissionDeadline=submissionDeadline,
-                reviewDeadline=reviewDeadline,
-                conferenceDate=conferenceDate
-                )
-        return JsonResponse(response_data)
-    return HttpResponse(status=405)
+        Conference.objects.create (
+            name=name ,
+            submissionDeadline=submissionDeadline ,
+            reviewDeadline=reviewDeadline ,
+            conferenceDate=conferenceDate
+        )
+        return JsonResponse ( response_data )
+    return HttpResponse ( status=405 )
 
 
 @csrf_exempt
 def add_new_review(request: HttpRequest):
     if request.method == 'POST':
         try:
-            ReviewService().add(request.body.decode('utf-8'))
-            return HttpResponse(status=200)
+            ReviewService ().add ( request.body.decode ( 'utf-8' ) )
+            return HttpResponse ( status=200 )
         except Exception as e:
-            return HttpResponse(e, status=400)
+            return HttpResponse ( e , status=400 )
     else:
-        return HttpResponse(status=405)
+        return HttpResponse ( status=405 )
 
 
 @csrf_exempt
 def get_all_reviews(request: HttpRequest):
     if request.method == 'GET':
         try:
-            reviews = ReviewService().getAll()
-            return JsonResponse(reviews.data, safe=False)
+            reviews = ReviewService ().getAll ()
+            return JsonResponse ( reviews.data , safe=False )
         except Exception as e:
-            return HttpResponse(e, status=400)
+            return HttpResponse ( e , status=400 )
     else:
-        return HttpResponse(status=405)
+        return HttpResponse ( status=405 )
 
 
 @csrf_exempt
 def add_new_bid(request: HttpRequest):
     if request.method == 'POST':
         try:
-            ReviewService().add_bid(request.body.decode('utf-8'))
-            return HttpResponse(status=200)
+            ReviewService ().add_bid ( request.body.decode ( 'utf-8' ) )
+            return HttpResponse ( status=200 )
         except Exception as e:
-            traceback.print_exc()
-            return HttpResponse(e, status=400)
+            traceback.print_exc ()
+            return HttpResponse ( e , status=400 )
     else:
-        return HttpResponse(status=405)
+        return HttpResponse ( status=405 )
 
 
 def get_conference_by_id(request):
     if request.method == 'GET':
-        conference_id = request.GET.get('id')
-        conference = Conference.objects.get(pk=conference_id)
-        conference_json = serializers.ConferenceSerializer(data=conference)
-        return JsonResponse(conference_json, safe=False)
-    return HttpResponse(status=405)
+        conference_id = request.GET.get ( 'id' )
+        conference = Conference.objects.get ( pk=conference_id )
+        conference_json = serializers.ConferenceSerializer ( data=conference )
+        return JsonResponse ( conference_json , safe=False )
+    return HttpResponse ( status=405 )
 
 
 def sign_up(request):
     if request.method == 'POST':
-        conference_id = request.POST.get('conference_id')
-        conference = Conference.objects.get(pk=conference_id)
-        participant_id = request.POST.get('participant_id')
-        participant = Participant.objects.get(pk=participant_id)
-        ConferenceAuthor.objects.create(
-                pEmail=participant.email,
-                cId=conference.pk
-                )
+        conference_id = request.POST.get ( 'conference_id' )
+        conference = Conference.objects.get ( pk=conference_id )
+        participant_id = request.POST.get ( 'participant_id' )
+        participant = Participant.objects.get ( pk=participant_id )
+        ConferenceAuthor.objects.create (
+            pEmail=participant.email ,
+            cId=conference.pk
+        )
         response_data = {
-            'pEmail': participant.email,
+            'pEmail': participant.email ,
             'cId': conference_id
-            }
-        return JsonResponse(response_data)
-    return HttpResponse(status=405)
+        }
+        return JsonResponse ( response_data )
+    return HttpResponse ( status=405 )
 
 
 def find_paper(request):
     if request.method == 'GET':
         try:
-            paper_id = request.GET.get('id')
-            paper = Conference.objects.get(pk=paper_id)
-            paper_json = serializers.PaperSerializer(data=paper)
-            return JsonResponse(paper_json, safe=False)
+            paper_id = request.GET.get ( 'id' )
+            paper = Conference.objects.get ( pk=paper_id )
+            paper_json = serializers.PaperSerializer ( data=paper )
+            return JsonResponse ( paper_json , safe=False )
         except Exception as e:
-            return HttpResponse(e, status=400)
+            return HttpResponse ( e , status=400 )
     else:
-        return HttpResponse(status=405)
+        return HttpResponse ( status=405 )
 
 
 @csrf_exempt
 def get_all_papers(request: HttpRequest):
     if request.method == 'GET':
         try:
-            paperService = PapersService()
-            papers = paperService.getAll()
-            return JsonResponse(papers.data, safe=False)
+            paperService = PapersService ()
+            papers = paperService.getAll ()
+            return JsonResponse ( papers.data , safe=False )
         except Exception as e:
-            return HttpResponse(e, status=400)
+            return HttpResponse ( e , status=400 )
     else:
-        return HttpResponse(status=405)
+        return HttpResponse ( status=405 )
 
 
 @csrf_exempt
 def add_new_paper(request: HttpRequest):
     if request.method == 'POST':
         try:
-            paperService = PapersService()
-            paperService.add(request.body.decode('utf-8'))
-            return HttpResponse(status=200)
+            paperService = PapersService ()
+            paperService.add ( request.body.decode ( 'utf-8' ) )
+            return HttpResponse ( status=200 )
         except Exception as e:
-            return HttpResponse(e, status=400)
+            return HttpResponse ( e , status=400 )
     else:
-        return HttpResponse(status=405)
+        return HttpResponse ( status=405 )
 
 
 @csrf_exempt
 def add_new_abstract(request: HttpRequest):
     if request.method == 'POST':
         try:
-            paperService = PapersService()
-            abstract = paperService.sendAbstract(request.body.decode('utf-8'))
-            paperService.add(abstract)
-            return HttpResponse(status=200)
+            paperService = PapersService ()
+            abstract = paperService.sendAbstract ( request.body.decode ( 'utf-8' ) )
+            paperService.add ( abstract )
+            return HttpResponse ( status=200 )
         except Exception as e:
-            return HttpResponse(e, status=400)
+            return HttpResponse ( e , status=400 )
     else:
-        return HttpResponse(status=405)
+        return HttpResponse ( status=405 )
+
+
+@csrf_exempt
+def file_upload(request: HttpRequest):
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['mile']
+        print ( uploaded_file.name )
+        print ( uploaded_file.size )
+        fs = FileSystemStorage ()
+        name = fs.save ( uploaded_file.name , uploaded_file )
+        context['url'] = fs.url ( name )
+    return render ( request , 'speaker-register.html' , context )
